9956098a71503b7fada79d56880bb58f324b0bc2
bjoberg <brett.oberg8@gmail.com>
2019-10-11 00:15:05 -0500

    Update unit tests

--- src/test/components/search/search.component.test.jsx
+++ src/test/components/search/search.component.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import Search from "../../../components/search/search.component";
-import { TextField } from "@material-ui/core";
+import { Input } from "antd";
 
 describe("[Search]", () => {
   let wrapper;
@@ -20,56 +20,38 @@ describe("[Search]", () => {
   });
 
   describe("[TreeView]", () => {
-    it("should render exaclty 1 <TextField /> component", () => {
+    it("should render exaclty 1 <Input /> component", () => {
       const expected = 1;
-      const actual = wrapper.find(TextField).length;
+      const actual = wrapper.find(Input).length;
       expect(actual).toStrictEqual(expected);
     });
 
-    it('should render <TextField /> with correct "id" prop', () => {
+    it('should render <Input /> with correct "id" prop', () => {
       const expected = "search-textfield";
-      const actual = wrapper.find(TextField).prop("id");
+      const actual = wrapper.find(Input).prop("id");
       expect(actual).toStrictEqual(expected);
     });
 
-    it('should render <TextField /> with correct "label" prop', () => {
-      const expected = "Search";
-      const actual = wrapper.find(TextField).prop("label");
-      expect(actual).toStrictEqual(expected);
-    });
-
-    it("should render <TextField /> with correct styles", () => {
+    it("should render <Input /> with correct styles", () => {
       const expected = true;
       const actual = wrapper
-        .find(TextField)
+        .find(Input)
         .hasClass(/(makeStyles)-(textField)-(\d+)/);
       expect(actual).toStrictEqual(expected);
     });
 
     it('should render <TextField /> with correct "value" prop', () => {
       const expected = value;
-      const actual = wrapper.find(TextField).prop("value");
+      const actual = wrapper.find(Input).prop("value");
       expect(actual).toStrictEqual(expected);
     });
 
     it("when <TextField /> changes, should call handleOnChange with correct params", () => {
       expect(handleOnChange.mock.calls.length).toStrictEqual(0);
       const e = { target: { value: "test" } };
-      wrapper.find(TextField).simulate("change", e);
+      wrapper.find(Input).simulate("change", e);
       expect(handleOnChange.mock.calls.length).toStrictEqual(1);
       expect(handleOnChange.mock.calls[0][0]).toStrictEqual(e);
-    });
-
-    it('should render <TextField /> with correct "margin" prop', () => {
-      const expected = "normal";
-      const actual = wrapper.find(TextField).prop("margin");
-      expect(actual).toStrictEqual(expected);
-    });
-
-    it('should render <TextField /> with correct "variant" prop', () => {
-      const expected = "outlined";
-      const actual = wrapper.find(TextField).prop("variant");
-      expect(actual).toStrictEqual(expected);
     });
   });
 });
